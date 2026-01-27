9adae2cbdc33658bb033b5feb05c4381d86581d8
chulkuji12 <chulkuji12@gmail.com>
2026-01-27 11:43:26 +0900

    Initial commit (goodSugar prjoect)

---
+++ goodSugar/src/main/java/edu/kh/goodsugar/GoodSugarApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.goodsugar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodSugarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodSugarApplication.class, args);
+	}
+
+}
