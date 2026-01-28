298d3367effcef505ebb23bc56ff724dc3738d6c
white6869 <white6869@naver.com>
2026-01-28 10:00:51 +0900

    20260128

--- good_sugar/src/components/Header.jsx
+++ good_sugar/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import '../styles/Header.css'
+import { Link } from "react-router-dom";
+import Logo from '../assets/LOGO.png'
 
 function Header() {
   return (
     <div className="header">
-
+      <Link to="/">
+        <img src={Logo} className="logo"/>
+      </Link>
     </div>
   )
 }
